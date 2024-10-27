294041c598ebdb1039fb84f20cc12ab745b59590
kazi-a <kazi.arafat@hotmail.com>
2024-10-27 19:15:44 -0400

    Add project files.

---
+++ ViewModels/ArtistViewModel.cs
@@ -0,0 +1,10 @@
+﻿using MusicStore.Models;
+
+namespace MusicStore.ViewModels
+{
+    public class ArtistViewModel
+    {
+        public List<Album> Album { get; set; } = new List<Album>();
+        public List<Artist> Artists { get; set; } = new List<Artist>();
+    }
+}
